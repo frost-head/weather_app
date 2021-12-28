422c0ccf22d206db4e753a5851d04fb6b6ed074d
Astra_stark <astha27sharma@gmail.com>
2021-12-28 17:14:38 +0530

    api integrated

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-tem=document.getElementById("temp")
+tem=document.getElementById('temp')
 cit=document.getElementById("city")
 average=document.getElementById("avg")
+
+// let response=fetch("https://api.weatherapi.com/v1/current.json?key=72e2a622adce48738e1153710210712&q=america&aqi=yes")
+
+
+async function fetchAsync(){
+    const response = await fetch("https://api.weatherapi.com/v1/current.json?key=72e2a622adce48738e1153710210712&q=mandi&aqi=yes")
+    const data = await response.json()
+    tem.innerText = data.current.temp_c + ' Degree Celcius'
+    cit.innerText= data.location.name
+    average.innerText=data.current.condition.text
+    
+}
+
+let temp = fetchAsync()
+
